09acebf3ff114e7a0a07905fe4b2b9c14ce27f92
Elitsa | Computer Science <estoynova2000@gmail.com>
2025-01-29 23:43:32 +0100

    Update new_calculator.py
    Edited the instructions

--- new_calculator.py
+++ new_calculator.py
@@ -29,31 +29,31 @@ operations = {
 def calculator():
     # print(art.logo)
     should_accumulate = True
-    #ex. 1 - Program asks the user to type the first number
+#ex. 1 - Program asks the user to type the first number
     num1 = float(input("What is the first number?: "))        #to be able to make the calculations, we need int or float!!
 
     while should_accumulate:
         for symbol in operations:        #to display the symbols as a choice
             print(symbol)
 
-    #ex. 2 - Program asks the user to type a mathematical operator (a choice of "+", "-", "*", & "/")
+#ex. 2 - Program asks the user to type a mathematical operator (a choice of "+", "-", "*", & "/")
         operation_symbol = input("Pick an operation: ")
 
-    #ex. 3 - Program asks the user to type the second number
+#ex. 3 - Program asks the user to type the second number
         num2 = float(input("What is the next number?: "))
 
-    #ex. 4 - Program works out the result based on the chosne mathematical operator
+#ex. 4 - Program works out the result based on the chosne mathematical operator
     #print(f"{operations[operation_symbol](num1, num2)}")
         answer = operations[operation_symbol](num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    #ex. 5 - Program asks if the user wants to continue working with the previous result
+#ex. 5 - Program asks if the user wants to continue working with the previous result
         choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.")
 
-        #ex. 6 - If yes, program loops to use the previous result as the first number and then repeats the calculation process.
+#ex. 6 - If yes, program loops to use the previous result as the first number and then repeats the calculation process.
         if choice == 'y':
             num1 = answer
-        # ex. 7 - If no, program asks the user for the first number again 
+# ex. 7 - If no, program asks the user for the first number again 
         else:
             should_accumulate = False
             print("\n" * 20)
